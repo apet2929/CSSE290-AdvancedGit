dae5994cc3bc3287dc63737fee7a88b259e2f3f3
apet2929 <realandrewpetersen@gmail.com>
2024-09-30 14:17:53 -0400

    Fix smash last 2

--- smash_last_two.rb
+++ smash_last_two.rb
@@ -2,16 +2,6 @@
 # example usage: ruby smash_last_two.rb
 
 `git stash -u` # ensure current changes aren't included in commit
-
-log = `git log --pretty=oneline | head -2`
-# example output: 
-# 02a20d58c80b7ca4cabff30369fd75107c60649b (HEAD -> main, origin/main, origin/HEAD) Remove hw folder
-# f06a6eaa493130a2574741d29d2729ff9d304dab Finish plumbing 2
-
-commits = log.split '\n'
-base_hash = commits.last.split(" ")[0]
-`git reset --soft #{commit_hashes.last}`
-`git commit `
-``
-
+`git reset --soft HEAD~2`
+`git commit -m "SMASH"`
 `git stash pop`
