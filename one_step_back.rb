937495942f991bd33bdbeae5dcffb43eeb8c8ef0
apet2929 <realandrewpetersen@gmail.com>
2024-09-30 14:21:26 -0400

    Fix one step back

--- one_step_back.rb
+++ one_step_back.rb
@@ -2,6 +2,5 @@
 
 # example usage: ruby one_step_back.rb
 `git stash -u -k`
-`git reset --mixed HEAD~` # staged changes become unstaged
-`git reset --soft HEAD~1` # changes from last commit become staged
-`git checkout HEAD~1`     # last commit becomes head
+`git reset`
+`git reset --soft HEAD~1`
