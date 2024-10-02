ec597afd60144bab6c6ad220b92adbccd1e50f1c
apet2929 <realandrewpetersen@gmail.com>
2024-10-02 15:07:18 -0400

    hi2

--- auto_merge_with_comments.rb
+++ auto_merge_with_comments.rb
@@ -5,7 +5,12 @@ trees = []
 output = nil
 OptionParser.new do |opts|
   opts.on("--use USE", "SHA of tree to include in history") do |val|
-<<<<<<< hi
+<<<<<<< branch1
+Here are some branchone changes
+=======
+Here's some branch2 changes
+That span several lines
+>>>>>>> branch2
     trees << val
   end
   opts.on("-o OUTPUT_BRANCH", "--output OUTPUT_BRANCH", "branch that contains alternative history") do |val|
