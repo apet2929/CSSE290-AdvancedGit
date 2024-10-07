90612e5196f9b97c83954a6ecf49018ccb04afe2
apet2929 <realandrewpetersen@gmail.com>
2024-10-07 16:33:36 -0400

    Auto merge with comments

--- auto_merge_with_comments.rb
+++ auto_merge_with_comments.rb
@@ -1,21 +1,30 @@
 #!/usr/bin/ruby
 require 'optparse'
 
-trees = []
-output = nil
+use = nil
+commitify = nil
+branch = nil
+prefix = nil
 OptionParser.new do |opts|
-  opts.on("--use USE", "SHA of tree to include in history") do |val|
-<<<<<<< branch1
-Here are some branchone changes
-=======
-Here's some branch2 changes
-That span several lines
->>>>>>> branch2
-    trees << val
+  opts.on("--use SHA", "this is the SHA of the commit that we want to use unmodified in case of conflicts") do |val|
+    use << val
   end
-  opts.on("-o OUTPUT_BRANCH", "--output OUTPUT_BRANCH", "branch that contains alternative history") do |val|
-    output = val
+  opts.on("--commitify SHA", "this the SHA of the commit we want to insert as comments only") do |val|
+    commitify = val
+  end
+  opts.on("--branch NAME", "this is the name of the branch where the output will go") do |val|
+    branch = val
+  end
+  opts.on("--prefix STRING", 'this is the prefix that will make a line a comment. So wed use "// " in C++ or "# " in ruby') do |val|
+    prefix = val
   end
 end.parse(ARGV)
-raise "You must specify a tree (-t)" if trees.empty?
-raise "You must an output branch" if output.nil?
+raise "You must specify all arguments!" if use.nil? or commitify.nil? or branch.nil? or prefix.nil?
+
+other_branch = "foobar"
+`git reset --hard #{use}`
+`git branch #{branch}`
+`git reset --hard #{commitify}`
+`git branch #{SecureRandom.hex(10)}`
+
+`git branch -d `
